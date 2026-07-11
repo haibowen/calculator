52178ca01f80191dbb66998f305b7484963fe0eb
hyplo <1461154748@qq.com>
2026-07-11 23:08:43 +0800

    测试添加4

--- app/src/main/java/com/example/myapplication/test/NewTest.java
+++ app/src/main/java/com/example/myapplication/test/NewTest.java
@@ -10,4 +10,9 @@ public class NewTest {
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
