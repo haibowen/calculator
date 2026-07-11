7b3f0c5e53521088a352adb51ddb60fb624fd006
hyplo <1461154748@qq.com>
2026-07-11 23:00:53 +0800

    测试添加2

--- app/src/main/java/com/example/myapplication/test/NewTest.java
+++ app/src/main/java/com/example/myapplication/test/NewTest.java
@@ -1,4 +1,13 @@
 package com.example.myapplication.test;
 
+import androidx.annotation.NonNull;
+
 public class NewTest {
+
+
+    @NonNull
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
